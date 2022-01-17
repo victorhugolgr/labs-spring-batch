9e8d013dc78901dd10e57daf375eea57b8b49e31
Victor Hugo <victorhugolgr@gmail.com>
2022-01-17 16:33:17 -0300

    Criação de um step chunk-based

---
+++ par-impar-job/src/test/java/br/com/victorhugolgr/parimparjob/ParImparJobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorhugolgr.parimparjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParImparJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
